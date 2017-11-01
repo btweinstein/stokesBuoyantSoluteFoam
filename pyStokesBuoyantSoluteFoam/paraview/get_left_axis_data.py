eac26ee917c2b314961b422daea0f2dda9ebdca6
bryan <btweinstein@gmail.com>
2017-11-01 17:46:21 -0400

    Ok, the code now works! Woohoo! We just need to figure out how to get the wall shear stress now...

--- pyStokesBuoyantSoluteFoam/paraview/get_left_axis_data.py
+++ pyStokesBuoyantSoluteFoam/paraview/get_left_axis_data.py
@@ -7,8 +7,15 @@ import sys
 import os
 import shutil
 
+# Parse the inputs
+simulation_dir = sys.argv[1]
+r_max = float(sys.argv[2])
+
+print simulation_dir
+print r_max
+
 # create a new 'OpenFOAMReader'
-simulation = OpenFOAMReader(FileName=sys.argv[1])
+simulation = OpenFOAMReader(FileName=simulation_dir)
 
 # Properties modified on j_m_colony_sweep_0p10foam
 simulation.MeshRegions = ['internalMesh', 'yeast_top', 'yeast_bottom', 'petri_top', 'petri_outer', 'petri_bottom']
@@ -18,11 +25,7 @@ simulation.CellArrays = ['U', 'c', 'p', 'p_rgh']
 slice1 = Slice(Input=simulation)
 
 # Properties modified on slice1.SliceType
-slice1.SliceType.Origin = [1.99952399730682, 0.0, 0.5]
-slice1.SliceType.Normal = [0.0, 1.0, 0.0]
-
-# Properties modified on slice1.SliceType
-slice1.SliceType.Origin = [1.99952399730682, 0.0, 0.5]
+slice1.SliceType.Origin = [r_max/2., 0.0, 0.5]
 slice1.SliceType.Normal = [0.0, 1.0, 0.0]
 
 
@@ -33,15 +36,33 @@ line_points = []
 axis_names = []
 
 line_tolerance = 3e-16
-### Left Axis ###
 
-axis_names.append('left_axis')
+### Left Axis ###
+axis_names.append('axis_left')
 
 left_line_points = [
     [0.0, 0.0, 0.0],
     [0.0, 0.0, 1.0]
 ]
 line_points.append(left_line_points)
+
+### Top Axis ###
+axis_names.append('axis_top')
+
+top_line_points = [
+    [0.0, 0.0, 1.0],
+    [r_max, 0.0, 1.0]
+]
+line_points.append(top_line_points)
+
+### Bottom Axis ###
+axis_names.append('axis_bottom')
+
+bottom_line_points = [
+    [0.0, 0.0, 0.0],
+    [r_max, 0.0, 0.0]
+]
+line_points.append(top_line_points)
 
 for cur_line_points, cur_axis_name in zip(line_points, axis_names):
     cur_line = PlotOverLine(Input=slice1, Source='High Resolution Line Source')
